054684538a6685d4f1cd9e33be83265c403c9c19
Dariusz Dwornikowski <dariusz.dwornikowski@cs.put.poznan.pl>
2013-02-16 14:42:07 +0100

    Petri net generated to dot file

--- petri_net.py
+++ petri_net.py
@@ -13,11 +13,35 @@ class PetriNet(object):
         self.transitions = alpha_model.footprint.seen
         self.inputs = alpha_model.ti
         self.outputs = alpha_model.to
-        self._make_structure(alpha_model.yl)
+        dot = self._make_structure(alpha_model.yl, alpha_model.ti, alpha_model.to)
+        with open("dot.dot", "w") as f:
+            f.write(dot)
 
-    def _make_structure(self,yl):
+
+    def _make_structure(self,yl,ti,to):
+        dot = []
+        dot.append("digraph pn {")
+        dot.append("rankdir=LR;")
         for elem in yl:
-            pass
+            for i in elem[0]:
+                dot.append('"%s" -> "P(%s)";' % (i, elem))
+                dot.append('"%s" [shape=box];' % i)
+                dot.append('"P(%s)" [shape=circle];' % str(elem))
+            for i in elem[1]:
+                dot.append('"P(%s)" -> "%s";' % (elem, i))
+                dot.append('"%s" [shape=box];' % i)
+        for i in ti:
+            dot.append('in -> "%s";' % i)
+        for o in to:
+            dot.append('"%s" -> out ;' % o)
+
+
+        dot.append("}")
+        dot = "\n".join(dot)
+        return dot
+
+
+
 
 
 
