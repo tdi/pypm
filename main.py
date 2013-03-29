799330c941df7ed012a25f7696eadeff41e70000
Dariusz Dwornikowski <dariusz.dwornikowski@cs.put.poznan.pl>
2013-03-29 13:03:30 +0100

    Initial commit

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ from simple_logs import read
 from footprint import Alpha
 from petri_net import PetriNet
 import sys
-import pprint
 
 
 def main(argv):
     log = read(argv[1])
     lm = Alpha(log)
-    print(lm)
     pn = PetriNet()
     pn.from_alpha(lm)
 
